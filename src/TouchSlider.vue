c77eecd6720f935b3e8c44be2516d807a8cb930d
Masaya Kazama <miyan@t-p.jp>
2018-01-21 16:22:36 +0900

    Remove console.log

--- src/TouchSlider.vue
+++ src/TouchSlider.vue
@@ -35,7 +35,6 @@ export default {
       e.preventDefault()
       const cw = e.target.clientWidth
       const rate = Math.max(0, Math.min(cw, e.touches[0].pageX - e.target.offsetLeft)) / cw
-      console.log(e.target.offsetLeft, e.touches[0].pageX, e.target.offsetLeft + cw, rate)
       this.emit(rate * this.range + this.min)
     }
   }
